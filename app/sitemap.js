f9ed0093ec1627b20dfb6e03114f6999736d1248
Lalan Kumar <lalankumarbnk17@gmail.com>
2026-09-22 06:45:13 +0530

    Add sitemap function for site URLs

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://lalan-electrical.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
