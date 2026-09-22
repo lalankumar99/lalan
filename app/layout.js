6c3f53b495286b10d402101259de6af070050bac
Lalan Kumar <lalankumarbnk17@gmail.com>
2026-09-22 06:49:27 +0530

    Add favicon icons to layout configuration
    Added icon configurations for favicon.

--- app/layout.js
+++ app/layout.js
@@ -45,6 +45,11 @@ export const metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+  icon: "/favicon.png",
+  shortcut: "/favicon.png",
+  apple: "/favicon.png",
+},
 };
 export default function RootLayout({ children }) {
   return (
