f7e9812dd982e850c358d9422fe9a72f4a318662
Lalan Kumar <lalankumarbnk17@gmail.com>
2026-09-22 06:45:49 +0530

    Add robots.js for SEO configuration

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://lalan-electrical.vercel.app/sitemap.xml",
+  };
+}
